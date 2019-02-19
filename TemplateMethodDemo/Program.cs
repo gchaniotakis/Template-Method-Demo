b6e79056527fa7e8bb5aa3c302d61ee0d453652a
George Chaniotakis <haniotinho@gmail.com>
2019-02-19 20:26:51 +0200

    Add project files.

---
+++ TemplateMethodDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TemplateMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
