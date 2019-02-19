a7cc4a61d40803d3bf24b5c437acbe66bca33b5e
George Chaniotakis <haniotinho@gmail.com>
2019-02-19 20:31:04 +0200

    Bread Class

---
+++ TemplateMethodDemo/Bread.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemplateMethodDemo
+{
+    public class Bread
+    {
+        public void Make()
+        {
+            var ingredients = GatherIngredients();
+            var dough = MixIngredients(ingredients);
+            var readyDough = Rest(dough);
+            var bread = Bake(readyDough);
+            var sliced = Slice(bread);
+
+        }
+    }
+}
